47c110219a52350ebca600b2a827440a4577d8d6
stephenmcveigh <smcveigh24@qub.ac.uk>
2017-11-17 12:13:48 +0000

    Fixed merge conflicts

--- Gage/app/src/main/java/uk/ac/qub/eeecs/game/OptionsScreen.java
+++ Gage/app/src/main/java/uk/ac/qub/eeecs/game/OptionsScreen.java
@@ -1,6 +1,5 @@
 package uk.ac.qub.eeecs.game;
 
-import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.Log;
@@ -39,7 +38,7 @@ public class OptionsScreen extends GameScreen {
     //define the buttons
     private PushButton mMenuButton;
     private PushButton mColourButton;
-    private PushButton mFlagButton;
+    private PushButton mChangeFlagButton;
 
     private float screen_height = 320.0f;
     private float screen_width = 480.0f;
@@ -77,14 +76,15 @@ public class OptionsScreen extends GameScreen {
 
         mMenuButton = new PushButton(100.0f, 50.0f, screen_width / 5, screen_height / 3,
                 "MenuButton", this );
-
-        mFlagButton = new PushButton(100.0f, 300.0f, screen_width / 5, screen_height / 3,
+        mChangeFlagButton = new PushButton(100.0f, 300.0f, screen_width / 5, screen_height / 3,
                 "FlagButton", this);
 
 
         mOptionsBackground = new GameObject(screen_width / 2,
                 screen_height / 2, screen_width, screen_height,
                 getGame().getAssetManager().getBitmap("OptionsBackground"), this);
+
+
 
     }
 
@@ -103,12 +103,12 @@ public class OptionsScreen extends GameScreen {
 
             // Update each button and transition if needed
             mMenuButton.update(elapsedTime);
-            mFlagButton.update(elapsedTime);
 
             if (mMenuButton.isPushTriggered())
                 changeToScreen(new MenuScreen(mGame));
-            if (mFlagButton.isPushTriggered()) {
-                mGame.setPreference("flag", !(mGame.getPreference("flag")));
+
+            if (mChangeFlagButton.isPushTriggered()) {
+                mGame.setPreference("Flag", !(mGame.getPreference("Flag")));
             }
         }
     }
@@ -126,13 +126,14 @@ public class OptionsScreen extends GameScreen {
     @Override
     public void draw(ElapsedTime elapsedTime, IGraphics2D graphics2D) {
         graphics2D.clear(Color.WHITE);
-        mMenuButton.draw(elapsedTime, graphics2D);
-        mFlagButton.draw(elapsedTime, graphics2D);
-        mOptionsBackground.draw(elapsedTime, graphics2D, mLayerViewport ,mScreenViewport);
+        mMenuButton.draw(elapsedTime, graphics2D, null, null);
+        mChangeFlagButton.draw(elapsedTime, graphics2D);
+
         //Paint the boolean flag value on screen
         Paint textPaint = new Paint();
         textPaint.setColor(Color.BLACK);
         textPaint.setTextSize(75);
         graphics2D.drawText(String.valueOf(mGame.getPreference("flag")), mMenuButton.getBound().getLeft(), mMenuButton.getBound().getBottom() + mMenuButton.getBound().getHeight() + 100, textPaint);
     }
+
 }
