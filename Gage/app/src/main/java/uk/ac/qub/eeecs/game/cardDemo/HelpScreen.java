5b010b01044ffb7b22c45fb08d29e1eee674f71e
eimlav <eimlav123@gmail.com>
2017-12-01 11:17:39 +0000

    Added PushButton to return to menu. Temporarily goes to CardDemoScreen until a MenuScreen class is actually implemented.

--- Gage/app/src/main/java/uk/ac/qub/eeecs/game/cardDemo/HelpScreen.java
+++ Gage/app/src/main/java/uk/ac/qub/eeecs/game/cardDemo/HelpScreen.java
@@ -1,13 +1,16 @@
 package uk.ac.qub.eeecs.game.cardDemo;
 
+import android.content.res.AssetManager;
 import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 import uk.ac.qub.eeecs.gage.Game;
+import uk.ac.qub.eeecs.gage.engine.AssetStore;
 import uk.ac.qub.eeecs.gage.engine.ElapsedTime;
 import uk.ac.qub.eeecs.gage.engine.graphics.IGraphics2D;
+import uk.ac.qub.eeecs.gage.ui.PushButton;
 import uk.ac.qub.eeecs.gage.util.GraphicsHelper;
 import uk.ac.qub.eeecs.gage.world.GameScreen;
 import uk.ac.qub.eeecs.gage.world.LayerViewport;
@@ -31,6 +34,11 @@ public class HelpScreen extends GameScreen {
     private InfoBar infoBar;
 
     /**
+     *
+     */
+    private PushButton menuScreenButton;
+
+    /**
      * Create a new game screen associated with the specified game instance
      *
      * @param game Game instance to which the game screen belongs
@@ -42,16 +50,37 @@ public class HelpScreen extends GameScreen {
         mLayerViewport = new LayerViewport();
         mScreenViewport = new ScreenViewport();
         GraphicsHelper.create3To2AspectRatioScreenViewport(game, mScreenViewport);
+
+        AssetStore assetManager = mGame.getAssetManager();
+
+        // Load in bitmaps
+        assetManager.loadAndAddBitmap("LeftArrow", "img/LeftArrow.png");
+        assetManager.loadAndAddBitmap("LeftArrowActive", "img/LeftArrowActive.png");
+
+        menuScreenButton = new PushButton(100,100,100,100, "LeftArrow", "LeftArrowActive", this);
     }
 
     @Override
     public void update(ElapsedTime elapsedTime) {
         infoBar.update(elapsedTime);
+        menuScreenButton.update(elapsedTime);
+
+        if(menuScreenButton.isPushTriggered()) changeToScreen(new CardDemoScreen(mGame));
     }
 
     @Override
     public void draw(ElapsedTime elapsedTime, IGraphics2D graphics2D) {
         infoBar.draw(elapsedTime, graphics2D, mLayerViewport, mScreenViewport);
+    }
+
+    /**
+     * Remove the current game screen and then change to the specified screen
+     *
+     * @param screen game screen to become active
+     */
+    private void changeToScreen(GameScreen screen) {
+        mGame.getScreenManager().removeScreen(this.getName());
+        mGame.getScreenManager().addScreen(screen);
     }
 }
 
