daaf15f417b2d2cc80f4e82844bcbea07a3f7821
stephenmcveigh <smcveigh24@qub.ac.uk>
2017-12-07 13:42:39 +0000

    Added the PackScreen class with constructor

---
+++ Gage/app/src/main/java/uk/ac/qub/eeecs/game/cardDemo/screens/PackScreen.java
@@ -0,0 +1,27 @@
+package uk.ac.qub.eeecs.game.cardDemo.screens;
+
+import uk.ac.qub.eeecs.gage.Game;
+import uk.ac.qub.eeecs.gage.engine.ElapsedTime;
+import uk.ac.qub.eeecs.gage.engine.graphics.IGraphics2D;
+import uk.ac.qub.eeecs.gage.world.GameScreen;
+
+/**
+ * Created by stephenmcveigh on 07/12/2017.
+ */
+
+public class PackScreen extends GameScreen {
+
+    public PackScreen(Game game) {
+        super("PackScreen", game);
+    }
+
+    @Override
+    public void update(ElapsedTime elapsedTime) {
+        
+    }
+
+    @Override
+    public void draw(ElapsedTime elapsedTime, IGraphics2D graphics2D) {
+
+    }
+}
