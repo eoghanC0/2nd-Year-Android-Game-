c3531f463273b411194abcc93eaa988c78720c2d
eimlav <eimlav123@gmail.com>
2018-01-22 16:55:34 +0000

    Performed major overhaul of HorizontalImageScroller. Now has proper functionality but is still WIP.

--- Gage/app/src/main/java/uk/ac/qub/eeecs/game/cardDemo/ui/HorizontalImageScroller.java
+++ Gage/app/src/main/java/uk/ac/qub/eeecs/game/cardDemo/ui/HorizontalImageScroller.java
@@ -2,13 +2,17 @@ package uk.ac.qub.eeecs.game.cardDemo.ui;
 
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.Vector;
 
 import uk.ac.qub.eeecs.gage.engine.AssetStore;
 import uk.ac.qub.eeecs.gage.engine.ElapsedTime;
 import uk.ac.qub.eeecs.gage.engine.graphics.IGraphics2D;
-import uk.ac.qub.eeecs.gage.util.GraphicsHelper;
+import uk.ac.qub.eeecs.gage.ui.PushButton;
 import uk.ac.qub.eeecs.gage.util.Vector2;
 import uk.ac.qub.eeecs.gage.world.GameObject;
 import uk.ac.qub.eeecs.gage.world.GameScreen;
@@ -31,18 +35,34 @@ public class HorizontalImageScroller extends GameObject {
     private ArrayList<Bitmap> bitmaps;
 
     /**
-     * Images currently displayed along with hidden images for one the scroller is activated
+     * Index of currently displayed bitmap
      */
-    private int[] displayedBitmapIndexes = {-1,-1,-1,-1,-1};
+    private int currentBitmapIndex = -1;
 
     /**
-     * Vector positions to draw displayed bitmaps
+     * Index of next bitmap to be displayed
      */
-    private Vector2[] displayedBitmapVectors = new Vector2[5];
+    private int nextBitmapIndex = -1;
+
     /**
-     * Prevents user from moving scroller
+     * Vector position of current bitmap
      */
-    private boolean lockScroller = false;
+    private Vector2 currentBitmapVector;
+
+    /**
+     * Vector position of current bitmap
+     */
+    private Vector2 nextBitmapVector;
+
+    /**
+     * Distance between bitmaps. Used to calculate how far to move
+     */
+    private float imageDistance = 0;
+
+    /**
+     * Distance moved by bitmaps
+     */
+    private float distanceMoved = 0;
 
     /**
      * Determines whether an animation is occuring
@@ -57,9 +77,15 @@ public class HorizontalImageScroller extends GameObject {
     private boolean scrollDirection = false;
 
     /**
-     * Internal matrix use to support draw requests
+     * Push buttons to change the bitmap displayed
      */
-    protected Matrix drawMatrix = new Matrix();
+    private PushButton pushButtonLeft;
+    private PushButton pushButtonRight;
+
+    /**
+     * Paint object for drawing bitmaps
+     */
+    private Paint mPaint = new Paint();
 
     // /////////////////////////////////////////////////////////////////////////
     // Constructors
@@ -78,36 +104,33 @@ public class HorizontalImageScroller extends GameObject {
         if(width < 0) width = 100;
         if(height < 0) height = 100;
         AssetStore assetManager = mGameScreen.getGame().getAssetManager();
+        assetManager.loadAndAddBitmap("Empty", "img/empty.png");
+        //assetManager.loadAndAddBitmap("TestRed", "img/TestImageRed.png");
         assetManager.loadAndAddBitmap("Test","img/help-image-test.png");
-
+        assetManager.loadAndAddBitmap("HIS Background", "img/his-background.png");
         bitmaps = new ArrayList<Bitmap>();
-        mBitmap = assetManager.getBitmap("Test");
+        mBitmap = assetManager.getBitmap("HIS Background");
 
         drawScreenRect.set((int) (position.x - mBound.halfWidth),
                 (int) (position.y - mBound.halfHeight),
                 (int) (position.x + mBound.halfWidth),
                 (int) (position.y + mBound.halfHeight));
 
-        for (int i = 0; i < 5; i++) {
-            displayedBitmapVectors[i] = new Vector2(0,0);
-        }
+        currentBitmapVector = new Vector2(position.x,position.y);
+        nextBitmapVector = new Vector2(position.x + imageDistance,position.y);
 
-        // TODO: Position to draw bitmap
-        Vector2 vector = new Vector2(0,0);
+        imageDistance = mBound.getWidth();
 
-        // Update positions of each bitmap
-        // Calculate best positions based on parameters passed in constructor
-        for (int i = 0; i < 5; i++) {
-            if(displayedBitmapIndexes[i] != -1) {
-                // TODO: Update vector for next bitmap
-                if(i < 2)
-                    vector.add(1,1);
-                else if(i == 2)
-                    vector.set(-2,-2);
-                else
-                    vector.add(1,1);
-            }
-        }
+        pushButtonLeft = new PushButton((mBound.getLeft() + (mBound.getWidth() * 0.25f)), position.y, mBound.getWidth() / 2, mBound.getHeight(), "Empty", gameScreen);
+        pushButtonRight = new PushButton((mBound.getRight() - (mBound.getWidth() * 0.25f)), position.y, mBound.getWidth() / 2, mBound.getHeight(), "Empty", gameScreen);
+
+        // Test images to determine scroller functions as intended
+        assetManager.loadAndAddBitmap("Red", "img/red.png");
+        assetManager.loadAndAddBitmap("Green", "img/green.png");
+        assetManager.loadAndAddBitmap("Blue", "img/blue.png");
+        addBitmap(assetManager.getBitmap("Red"));
+        addBitmap(assetManager.getBitmap("Green"));
+        addBitmap(assetManager.getBitmap("Blue"));
 
     }
 
@@ -121,83 +144,113 @@ public class HorizontalImageScroller extends GameObject {
      */
     public void addBitmap(Bitmap bitmap) {
         if(bitmap != null) {
+            if(bitmaps.size() == 0) currentBitmapIndex = 0;
             bitmaps.add(bitmap);
-
-            for (int i = 0; i < 5; i++) {
-                if(displayedBitmapIndexes[i] == -1) displayedBitmapIndexes[i] = bitmaps.size() - 1;
-                break;
-            }
         }
     }
 
     /**
-     * Updates the currently displayed bitmaps and hidden bitmaps
-     * Should be called after animation moving images has completed
-     * @param direction Specifies direction to move in (true = right false = left)
+     * Gets the next bitmap in the bitmaps ArrayList based on the direction
+     * @param direction
      */
-    private void updateDisplayedBitmaps(boolean direction) {
-        // Move everything in specified direction
-        if(!bitmaps.isEmpty() && bitmaps.size() > 1) {
+    private void getNextBitmap(boolean direction) {
+        if(bitmaps.size() > 1) {
             int directionInt = direction ? 1 : -1;
-            for (int i = 0; i < 5; i++) {
-                displayedBitmapIndexes[i] = (displayedBitmapIndexes[i] + bitmaps.size() + directionInt) % bitmaps.size();
-            }
+            nextBitmapIndex = (currentBitmapIndex + bitmaps.size() + directionInt) % bitmaps.size();
         }
     }
 
     @Override
     public void update(ElapsedTime elapsedTime) {
-        // TODO: Check for touch to change picture
+        super.update(elapsedTime);
+        pushButtonLeft.update(elapsedTime);
+        pushButtonRight.update(elapsedTime);
+        boolean leftPushed = pushButtonLeft.isPushTriggered();
+        boolean rightPushed = pushButtonRight.isPushTriggered();
 
-        // Set animation in motion if a touch is detected
-        if(/*Touch event triggered*/true && !animationTriggered) {
+        if(bitmaps == null) return;
+
+        // Check for input to determine if animation should be triggered to move the bitmaps
+        if((leftPushed || rightPushed) && !animationTriggered) {
             animationTriggered = true;
 
-            // Set direction to scroll images
-            if(/*touch/ left side/swipe left*/true)
-                scrollDirection = false;
-            else
+            // Set direction to scroll images and vector of next bitmap
+            if(leftPushed) {
                 scrollDirection = true;
+                nextBitmapVector = new Vector2(currentBitmapVector.x - imageDistance, currentBitmapVector.y);
+            }
+            else {
+                scrollDirection = false;
+                nextBitmapVector = new Vector2(currentBitmapVector.x + imageDistance, currentBitmapVector.y);
+            }
+
+            getNextBitmap(scrollDirection);
+
+            // Cancel animation if there is no other bitmap in bitmaps
+            if(nextBitmapIndex == -1) animationTriggered = false;
+
+            // Reset distance moved to base value of 0
+            distanceMoved = 0;
+            Log.d("DEBUG", "Starting animation moving " + (leftPushed ? "left" : "right") + " Current bitmap:" + currentBitmapIndex + " Next bitmap: " + nextBitmapIndex);
+
         }
 
-        // Perform animation
+        // Perform animation if an animation has been triggered
         if(animationTriggered) {
             // TODO: Animation
+            boolean isAnimationComplete = false;
 
-            if(/* Animation complete */ true) {
+            Log.d("DEBUG", "CurrentBitmapVector: " + currentBitmapVector.x +  "," + currentBitmapVector.y + " NextBitmapVector: " + nextBitmapVector.x + "," + nextBitmapVector.y);
+
+            // Move current bitmap and next bitmap
+            float moveBy = 0;
+            if(!scrollDirection) moveBy = -1 * imageDistance * 0.05f;
+            else moveBy = imageDistance * 0.05f;
+
+            currentBitmapVector.add(moveBy, 0);
+            nextBitmapVector.add(moveBy, 0);
+
+            distanceMoved += Math.abs(moveBy);
+            Log.d("DEBUG", "Distancemoved: " + distanceMoved + " imagedistance: " + imageDistance);
+            if(distanceMoved >= imageDistance) isAnimationComplete = true;
+            // System.nanoTime() - animationStartTime > (moveTime * 1e+9)
+
+            if(isAnimationComplete) {
                 animationTriggered = false;
-                updateDisplayedBitmaps(scrollDirection);
+                currentBitmapIndex = nextBitmapIndex;
+                currentBitmapVector = new Vector2(position.x, position.y);
+                distanceMoved = 0;
+                Log.d("DEBUG", "Finished animation " + " Current bitmap:" + currentBitmapIndex);
+                Log.d("DEBUG", "Final CurrentBitmapVector: " + currentBitmapVector.x +  "," + currentBitmapVector.y + " NextBitmapVector: " + nextBitmapVector.x + "," + nextBitmapVector.y);
             }
         }
-        super.update(elapsedTime);
+
 
     }
 
     @Override
     public void draw(ElapsedTime elapsedTime, IGraphics2D graphics2D, LayerViewport layerViewport, ScreenViewport screenViewport) {
         // TODO: Get the damn thing to actually draw something
-        /*if (GraphicsHelper.getClippedSourceAndScreenRect(this, layerViewport, screenViewport, drawSourceRect, drawScreenRect)) {
-
-            float scaleX = (float) drawScreenRect.width() / (float) drawSourceRect.width();
-            float scaleY = (float) drawScreenRect.height() / (float) drawSourceRect.height();
-
-            // Build an appropriate transformation matrix
-            drawMatrix.reset();
-            drawMatrix.postScale(scaleX, scaleY);
-            drawMatrix.postRotate(0, scaleX * mBitmap.getWidth()
-                    / 2.0f, scaleY * mBitmap.getHeight() / 2.0f);
-            //drawMatrix.postTranslate(100, drawScreenRect.top);
-            drawMatrix.setScale(1f, 1f);
-            // Draw the image
-            graphics2D.drawBitmap(mBitmap, drawMatrix, null);
-        }*/
-
         // TODO: Draw bitmaps
-        for (int i = 0; i < 5; i++) {
-            // Draw bitmap i.e. graphics2D.drawBitmap(bitmaps[displayedBitmapIndexes[i]], ...);
-        }
-
         super.draw(elapsedTime, graphics2D);
+
+        if(currentBitmapIndex == -1) return;
+
+        Rect currentBitmapRect = new Rect();
+        currentBitmapRect.set((int) (currentBitmapVector.x - mBound.halfWidth * 0.5),
+                (int) (position.y - mBound.halfHeight),
+                (int) (currentBitmapVector.x + mBound.halfWidth * 0.5),
+                (int) (position.y + mBound.halfHeight));
+        graphics2D.drawBitmap(bitmaps.get(currentBitmapIndex), null, currentBitmapRect, null);
+
+        if(nextBitmapIndex == -1) return;
+        Rect nextBitmapRect = new Rect();
+        nextBitmapRect.set((int) (nextBitmapVector.x - mBound.halfWidth * 0.5),
+                (int) (position.y - mBound.halfHeight),
+                (int) (nextBitmapVector.x + mBound.halfWidth * 0.5),
+                (int) (position.y + mBound.halfHeight));
+        graphics2D.drawBitmap(bitmaps.get(nextBitmapIndex), null, nextBitmapRect, null);
+
     }
 
 
